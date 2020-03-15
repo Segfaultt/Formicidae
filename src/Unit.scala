328708a50612e0e4315f8102f46879fc7d0e3237
Mike <michael.hallam3@gmail.com>
2020-03-15 12:52:57 +0000

    Added entity traits Added terrain traits

--- src/Unit.scala
+++ src/Unit.scala
@@ -1,8 +1,14 @@
 case class Position(var x: Int, var y: Int, var z: Int)
 
-trait Unit {
+trait Entity {
   def name: String
   def hp: Int
   def pos: Position
+  def dmg: Int
+}
 
+trait Terrain {
+  def dimensions: Position
+  def entity(pos: Position): Entity
+  def groundExist(pos: Position): Boolean
 }
