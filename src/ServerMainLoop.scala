0043c878e3b27004386f6c89fafd2aa96dbf8ef7
Mike <michael.hallam3@gmail.com>
2020-03-15 14:56:54 +0000

    Renamed main, provided methods to allow for updates to be sent to the 'monolithic' terrain object

--- src/ServerMainLoop.scala
+++ src/ServerMainLoop.scala
@@ -9,32 +9,42 @@ import traits._
 
 object MainLoop {
 
-  private val tick: Int = 500 // how long a 'tick' of the clock should be in ms
-  private var te: Terrain = null // the terrain object that stores the state of the game
+	private val tick: Int = 500 // how long a 'tick' of the clock should be in ms
+	private var te: Terrain = null // the terrain object that stores the state of the game
+	private var moves: List[Move] = Nil
+	
+	def init: Unit = 
+	{
+	  val seed: Int = getRand
+	  ter = generateTerrain(getRand)
+	  
+	}
+	
+	def mainLoop: Unit = 
+	{
+	  var quit: Boolean = false
+	
+	  while(!quit)
+	  {
+	    val todo: List[Move] = getMove
+	  }
+	}
+	
+	def getMove: List[Move] = 
+	{
+		val re: List[Move] = moves
+		moves = Nil
+		re
+	}
 
-  def init: Unit = 
-  {
-    val seed: Int = getRand
-    ter = generateTerrain(getRand)
-    
-  }
-
-  def mainLoop: Unit = 
-  {
-    var quit: Boolean = false
-
-    while(!quit)
-    {
-      // TODO - main loop here
-    }
-  }
-
-  // fetches random enough number
-  // doesn't have to be cryptographically random
-  def getRand: Int = 5 // use get time
-
-  def generateTerrain(seed: Int): Terrain = 
-  {
-    BattleGround.generateTerrain
-  }
+	def addMove(x: Move): Unit = (moves = x::moves)
+	
+	// fetches random enough number
+	// doesn't have to be cryptographically random
+	def getRand: Int = 5 // use get time
+	
+	def generateTerrain(seed: Int): Terrain = 
+	{
+	  BattleGround.generateTerrain
+	}
 }
