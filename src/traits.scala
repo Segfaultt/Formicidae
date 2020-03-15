7414eb91f006c0b06e789af24301a013d4345510
Luca Pengelly <xophnex@protonmail.ch>
2020-03-15 14:47:41 +0000

    Formatting changes

--- src/traits.scala
+++ src/traits.scala
@@ -2,11 +2,12 @@ case class Position(var x: Int, var y: Int, var z: Int)
 // 0 index ur position
 
 trait Entity {
-  def name: String
-  def hp: Double
-  def pos: Position
-  def dmg: Double
-  def speed: Double
+	def name: String
+	def hp: Double
+	def pos: Position
+	def dmg: Double
+	def speed: Double
+	def inventory: Array[Material]
 }
 
 /*
@@ -17,16 +18,16 @@ trait Entity {
  */
 
 trait Terrain {
-  def dimensions: Position
-  def entity(pos: Position): Entity
-  def moveEntity(origin: Position, end: Position): Boolean // true if move possible and complete otherwise false
-  def groundExist(pos: Position): Boolean
-  def groundType(pos: Position): Material
-  def generateTerrain(seed: Int): Unit
+	def dimensions: Position
+	def entity(pos: Position): Entity
+	def moveEntity(origin: Position, end: Position): Boolean // true if move possible and complete otherwise false
+	def groundExist(pos: Position): Boolean
+	def groundType(pos: Position): Material
+	def generateTerrain(seed: Int): Unit
 }
 
 trait Material {
-  def hardness: Int
-  def output: Array[Material]
-  def movementModifier(sp: Double): Double
+	def hardness: Int
+	def output: Array[Material]
+	def movementModifier(sp: Double): Double
 }
