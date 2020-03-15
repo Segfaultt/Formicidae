4ac639f727693df8561afe24e52bef80d1dbbb3d
Luca Pengelly <xophnex@protonmail.ch>
2020-03-15 15:03:28 +0000

    Added transmitter

---
+++ src/Transmitter.scala
@@ -0,0 +1,8 @@
+import java.net.Socket
+import java.net.InetSocketAddress
+import java.net.SocketAddress
+import java.nio.channels.DatagramChannel
+import java.nio.channels.ServerSocketChannel
+
+object Transmitter {
+}
