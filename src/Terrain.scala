686ee5cb25ca30fc2d4e2681d8479844307c94bd
Mike <michael.hallam3@gmail.com>
2020-03-16 15:17:38 +0000

    Added methods to move entities

--- src/Terrain.scala
+++ src/Terrain.scala
@@ -1,13 +1,20 @@
-import traits 
+import com._
 
-object BattleGround extends Terrain (dimensions : Position) {
-	val entities = new Array[Entity](dimensions.x)(dimensions.y)(dimensions.z) 
+object BattleGround extends Terrain {
+	private val entities = Array.ofDim[Entity](dimensions.x,dimensions.y,dimensions.z) 
         // implementation clear internally: DO NOT EXPOSE WITHOUT MAKING CLEAR
-	val materials = new Array[Material](dimensions.x)(dimensions.y)(dimensions.z)
+	private val materials = Array.ofDim[Material](dimensions.x,dimensions.y,dimensions.z)
+        private var dim: Position = null
 
-        def dimensions: Position = dimensions
+        def init(dimensions: Position): Unit = 
+        {
+          dim = dimensions
+        }
+
+        def dimensions: Position = dim
         def entity(pos: Position): Entity = entities(pos.x)(pos.y)(pos.z)
-        def groundExist(pos: Position): Boolean = materials(pos.x)(pos.y)(pos.z)
+        def groundExist(pos: Position): Boolean = null != materials(pos.x)(pos.y)(pos.z)
+        // null used to represent no terrain
         def groundType(pos: Position): Material = materials(pos.x)(pos.y)(pos.z)
 
         def updateTerrain(updates: Array[(Position,Material)]): Unit = 
@@ -23,7 +30,7 @@ object BattleGround extends Terrain (dimensions : Position) {
           }
         }
 
-        def updateEntities(updates: Array[(Position,Entities)]: Unit = 
+        def updateEntities(updates: Array[(Position,Entity)]): Unit = 
         {
           var apos: Int = 0
           val max: Int = updates.size
@@ -34,4 +41,14 @@ object BattleGround extends Terrain (dimensions : Position) {
             apos+=1
           }
         }
+
+        private def getEnt(pos: Position): Entity = entities(pos.x)(pos.y)(pos.z)
+        private def setEnt(pos: Position, ent: Entity): Unit = entities(pos.x)(pos.y)(pos.z) = ent
+
+        def moveEntity(origin: Position, end: Position): Boolean = 
+        {
+          setEnd(end,getEnt(origin))
+        }
+
+        def generateTerrain(seed: Int): Unit = ??? // TODO - implement initial terrain generation
 }
